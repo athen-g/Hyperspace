c1302e861894a886ac42faf30989881e4da78520
Atharva Ghule <atharvachamp45@gmail.com>
2026-08-02 17:05:54 +0530

    revert: recombine the 5 split blogs back into one single craftingVisualIdentity blog

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -4,18 +4,10 @@ import BlogPostLayout from './blog/BlogPostLayout';
 import BlogComingSoon from './BlogComingSoon';
 
 // Registry of built blog posts keyed by their slug
-import blogPart1 from '../content/posts/blog_part1_initiate_calibration';
-import blogPart2 from '../content/posts/blog_part2_activate_immersion';
-import blogPart3 from '../content/posts/blog_part3_render_creation';
-import blogPart4 from '../content/posts/blog_part4_brand_universe';
-import blogPart5 from '../content/posts/blog_part5_design_language';
+import craftingVisualIdentity from '../content/posts/craftingVisualIdentity';
 
 const POSTS_REGISTRY = {
-  [blogPart1.slug]: blogPart1,
-  [blogPart2.slug]: blogPart2,
-  [blogPart3.slug]: blogPart3,
-  [blogPart4.slug]: blogPart4,
-  [blogPart5.slug]: blogPart5,
+  [craftingVisualIdentity.slug]: craftingVisualIdentity,
 };
 
 const BlogPage = () => {
